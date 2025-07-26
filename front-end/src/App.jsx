4371b83997677b3dfb171b2d855a7c25f12c047e
Tamra Chan <tamrachan@gmail.com>
2025-07-26 20:37:25 +0100

    Added sample events card

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -5,6 +5,7 @@ import Feedback from './pages/Feedback';
 import {Routes, Route} from "react-router-dom";
 import NavBar from './components/NavBar';
 import Contribution from './pages/Contribution';
+import Events from './pages/Events';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />}/>
           <Route path="/recipes" element={<Recipes />}/>
           <Route path="/contribution" element={<Contribution />}/>
+          <Route path="/events" element={<Events />}/>
           <Route path="/feedback" element={<Feedback />}/>
         </Routes>
       </main>
--- front-end/src/pages/Events.jsx
+++ front-end/src/pages/Events.jsx
@@ -1,1 +1,23 @@
-// TODO
+import "../css/Events.css";
+
+function Events() {
+
+    return (
+        <div className="events">
+            <h1>Events</h1>
+            
+            <p>View upcoming events:</p>
+            <div className="events-grid">
+                <div className="events-card">
+                    <h3>EVENT TITLE</h3>
+                    <h4>Date: </h4>
+                    <h4>Time: </h4>
+                    <h4>Cost: </h4>
+                    <p className="events-desc">Event description...</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Events;
