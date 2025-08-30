1bebd254225a8f83444cb119cceac8cfa902f7c7
Tamra Chan <tamrachan@gmail.com>
2025-08-30 21:09:34 +0100

    Added linktree and discord to the footer

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -8,8 +8,14 @@ function Footer() {
     <footer className="site-footer">
       <div className="footer-content">
         <div className="footer-links">
+          <a href="https://linktr.ee/standrewsbakingsoc" target="_blank" rel="noopener noreferrer">
+            Linktree
+          </a>
           <a href="https://www.instagram.com/standrewsbakingsoc" target="_blank" rel="noopener noreferrer">
             Instagram
+          </a>
+          <a href="https://discord.gg/cVEEPpAney" target="_blank" rel="noopener noreferrer">
+            Discord
           </a>
           <a href="mailto:bakingsoc@st-andrews.ac.uk">
             bakingsoc@st-andrews.ac.uk
