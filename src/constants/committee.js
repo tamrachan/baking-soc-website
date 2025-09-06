da8b3a792a11007caa3fe8fac9a5b140c8ff02dc
Tamra Chan <tamrachan@gmail.com>
2025-09-06 16:33:59 +0100

    Updated images path for deployment

--- src/constants/committee.js
+++ src/constants/committee.js
@@ -2,37 +2,37 @@ export const committeeMembers = [
     {
         name: 'Yi Xin Chong',
         role: 'President',
-        image: '/committee-images/yixin.jpg',
+        image: import.meta.env.BASE_URL + 'committee-images/yixin.jpg',
         bio: "Hey bakers, my name is Yi Xin (pronounced yee-sin) and I will be your president this year!! Since secondary, I have been occasionally baking as a hobby, and one of my favourite things to bake is a red velvet cupcake with cream cheese icing. I'm so excited to meet some fellow bakers and see what people will be baking up at our lessons! And most importantly, come join and have fun at all of our events, which will be suitable for bakers of all skill levels!!"
     },
     {
         name: 'Hanaa Khan',
         role: 'Vice President',
-        image: '/committee-images/hanaa.jpg',
+        image: import.meta.env.BASE_URL + 'committee-images/hanaa.jpg',
         bio: "Hi everyone! I'll be the VP of bakesoc! If u have any questions, lmk! My role is super broad, so I'll do my best to help you all out - I'm so excited to bake with you all!"
     },
     {
         name: 'Bao Thy Nguyen',
         role: 'Events Coordinator',
-        image: '/committee-images/thy.jpg',
+        image: import.meta.env.BASE_URL + 'committee-images/thy.jpg',
         bio: "Hi!! You can just call me by my nickname T. I manage the society's Instagram account & create all the content you see like the animation promo & committee posts 🐈‍⬛ I'll also be helping to facilitate the society's future events together with our committee. Can't wait to meet you all~~ whether you love to bake, want to learn or just want to hangout, everyone is welcome to join. I hope you'll have lots of fun at our future events and make many good memories."
     },
     {
         name: 'Ona Wright',
         role: 'Treasurer',
-        image: '/committee-images/ona.jpg',
+        image: import.meta.env.BASE_URL + 'committee-images/ona.jpg',
         bio: "Hello~ my name is Ona Wright and I'm the Treasurer for the Baking Society. I'm one of the few members who hasn't done a lot of baking, so come find me if you need someone to commiserate... or of course if you have any questions about the society! (I will say though that I am an expert at 'getting a sweet treat' so hopefully that counts for something hehehe~)"
     },
     {
         name: 'Zoya Singh',
         role: 'Secretary',
-        image: '/committee-images/zoya.jpg',
+        image: import.meta.env.BASE_URL + 'committee-images/zoya.jpg',
         bio: "Hi:) I'm Zoya and I'm thrilled to be part of the baking society as secretary - despite being a walking hazard in the kitchen (just ask anyone). Having friends and family who share a love for baking, I've learnt that it's more than just recipe-following, but also a way for bringing people together, sharing laughter and making good memories"
     },
     {
         name: 'Tamra Chan',
         role: 'Website Developer/Manager',
-        image: '/committee-images/tamra.jpg',
+        image: import.meta.env.BASE_URL + 'committee-images/tamra.jpg',
         bio: "Hi, I'm Tamra - the future website manager for Baking Soc and a direct entry student going into third year! I will look after the Baking Soc website, making sure everything is freshly updated, easy to navigate, and running smoothly. If you're wondering about the cookies on the website, don't worry, I've already eaten them for you :). I also have a nut allergy, so allergy awareness is important to me. Feel free to contact me about the upcoming website, allergies, or uni in general - I'm always happy to help!"
     }
 ];
