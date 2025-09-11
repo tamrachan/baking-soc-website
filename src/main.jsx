eaf58b3e910db1b1ec3278a65e9795b8eeba9aed
Tamra Chan <tamrachan@gmail.com>
2025-09-11 19:28:20 +0100

    Fixed main.jsx for custom domain

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/baking-soc-website">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
