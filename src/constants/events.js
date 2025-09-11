6d4d9131b2dc2cb653cfa5dc78c28a61afef7634
Tamra Chan <tamrachan@gmail.com>
2025-09-11 19:49:09 +0100

    Updated events for Tuesday

--- src/constants/events.js
+++ src/constants/events.js
@@ -2,19 +2,10 @@ export const upcomingEvents = [
     {
         id: 1,
         title: "Making cookie dough 1 (Freshers session)",
-        date: "Wednesday 17th September, 2025",
+        date: "Tuesday 16th September, 2025",
         time: "4:00 PM - 5:00 PM",
         cost: "FREE",
-        location: "Holy Trinity Church/Small Rehearsal Room",
+        location: "Chaplaincy (opposite the Union)",
         description: "A hands-on workshop where you can meet other friendly bakers and make edible cookie dough!"
     },
-    {
-        id: 2,
-        title: "Making cookie dough 2 (Freshers session)",
-        date: "Friday 19th September, 2025",
-        time: "4:00 PM - 5:00 PM",
-        cost: "FREE",
-        location: "Holy Trinity Church/Small Rehearsal Room",
-        description: "Another hands-on workshop where you can meet other friendly bakers and make edible cookie dough!"
-    }
 ];
