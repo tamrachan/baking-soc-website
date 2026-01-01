20c77117acafe7d599fe6c59fc78974ea255ec07
Tamra Chan <tamrachan@gmail.com>
2026-01-01 16:47:42 +0000

    Updated committee members and recipes

--- src/constants/committee.js
+++ src/constants/committee.js
@@ -18,16 +18,40 @@ export const committeeMembers = [
         bio: "Hi!! You can just call me by my nickname T. I manage the society's Instagram account & create all the content you see like the animation promo & committee posts 🐈‍⬛ I'll also be helping to facilitate the society's future events together with our committee. Can't wait to meet you all~~ whether you love to bake, want to learn or just want to hangout, everyone is welcome to join. I hope you'll have lots of fun at our future events and make many good memories."
     },
     {
-        name: 'Ona Wright',
+        name: 'Maxence Morel Dierckx',
         role: 'Treasurer',
-        image: import.meta.env.BASE_URL + 'committee-images/ona.jpg',
-        bio: "Hello~ my name is Ona Wright and I'm the Treasurer for the Baking Society. I'm one of the few members who hasn't done a lot of baking, so come find me if you need someone to commiserate... or of course if you have any questions about the society! (I will say though that I am an expert at 'getting a sweet treat' so hopefully that counts for something hehehe~)"
+        image: import.meta.env.BASE_URL + 'committee-images/max.jpeg',
+        bio: "Hi! Let's have fun this year."
     },
     {
         name: 'Zoya Singh',
         role: 'Secretary',
         image: import.meta.env.BASE_URL + 'committee-images/zoya.jpg',
         bio: "Hi:) I'm Zoya and I'm thrilled to be part of the baking society as secretary - despite being a walking hazard in the kitchen (just ask anyone). Having friends and family who share a love for baking, I've learnt that it's more than just recipe-following, but also a way for bringing people together, sharing laughter and making good memories"
+    },
+    {
+        name: 'Grace Langford',
+        role: 'Events Coordinator',
+        image: import.meta.env.BASE_URL + 'committee-images/grace.jpeg',
+        bio: "Hi! I’m Grace, a second-year who usually lives in jumpers — but here’s a rare moment of me all dressed up (thank you, prom). I’m a big fan of almond croissants and chocolate chip cookies (sweet things just hit different) and I’m super excited to be this year’s Event Coordinator! Looking forward to a year of cosy bakes, new faces, and maybe a little controlled kitchen chaos ✨ Always happy to chat, so don’t hesitate to say hi! :))"
+    },
+    {
+        name: 'Julija Hoyer',
+        role: 'Logistics Coordinator',
+        image: import.meta.env.BASE_URL + 'committee-images/julija.jpeg',
+        bio: "Hi I am Julia and I am the new logistics officer. I ensure we have all the ingredients and equipment we might need for our baking sessions to run smoothly. I love to bake, especially when I have an assignment due the next day, which I should probably work on. Ops. :)"
+    },
+    {
+        name: 'Sineidin Tamakloe',
+        role: 'Baking Lead',
+        image: import.meta.env.BASE_URL + 'committee-images/sineidin.jpeg',
+        bio: "Hi, my name's Sineidin and Im the Baking Society's Baking Lead. I've always had a love for baking and I'm really excited to share lots of recipes and bake with new people :)"
+    },
+    {
+        name: 'Thomas Yonaha-Mccoy',
+        role: 'Freshers rep',
+        image: import.meta.env.BASE_URL + 'committee-images/thomas.jpeg',
+        bio: "Hi I'm Thomas, as your freshers rep this year I will be yapping excessively so feel free to ask if you have any worries or concerns!"
     },
     {
         name: 'Tamra Chan',
--- src/constants/events.js
+++ src/constants/events.js
@@ -1,11 +1,11 @@
 export const upcomingEvents = [
     {
         id: 1,
-        title: "Mini Cheesecake Bake Session",
-        date: "Monday 13th October, 2025",
-        time: "5:30 PM - 7:00 PM",
-        cost: "£2 - members, £4 - non-members",
-        location: "Holy Trinity Church Hall",
-        description: "Join us to bake some delicious cheesecake and meet new people!"
+        title: "More sessions coming soon for Semester 2!",
+        date: "TBA",
+        time: "TBA",
+        cost: "TBA",
+        location: "TBA",
+        description: "Join our events in Semester 2 to bake some treats and meet new people!"
     },
 ];
